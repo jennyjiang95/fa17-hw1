0e2a3d7408bf32f8a243e0e1e2045333897a10e2
jennyjiang95 <jennyjiang95@berkeley.edu>
2017-09-17 19:04:23 -0700

    finish hw1

--- foobar.rb
+++ foobar.rb
@@ -3,6 +3,13 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    array = Array.new
+    
+    a = a.map{|elem| (elem.to_i) + 2}
+
+    a.map{|i| array.push(i) if i.even?}
+
+    array = array.uniq.select{|j| j <= 10}.inject(:+)
   end
 end
 
--- hw1.rb
+++ hw1.rb
@@ -4,12 +4,12 @@ def squared_sum(a, b)
 end
 
 def sort_array_plus_one(a)
-  # Q2 CODE HERE
+  a.sort.map{|i| i + 1}
 
 end
 
 def combine_name(first_name, last_name)
-  # Q3 CODE HERE
+  first_name + " " + last_name
 
 end
 
